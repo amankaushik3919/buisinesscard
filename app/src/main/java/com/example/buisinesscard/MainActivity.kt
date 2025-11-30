bec9d34f97d2d1291387e62383c015349d5dca2b
AMAN KAUSHIK <84031878+amankaushik3919@users.noreply.github.com>
2025-11-30 21:11:53 +0530

    detail Updated

--- app/src/main/java/com/example/buisinesscard/MainActivity.kt
+++ app/src/main/java/com/example/buisinesscard/MainActivity.kt
@@ -110,7 +110,7 @@ fun ContactInfoSection(modifier: Modifier = Modifier) {
         ContactInfoRow(
             image = painterResource(id = R.drawable.callicon),
             iconDescription = "Call Icon",
-            text = "+91 8882050710"
+            text = "+91 1234567890"
         )
         ContactInfoRow(
             image = painterResource(id=R.drawable.insta),
@@ -120,7 +120,7 @@ fun ContactInfoSection(modifier: Modifier = Modifier) {
         ContactInfoRow(
             image = painterResource(id = R.drawable.mail),
             iconDescription = "Email Icon",
-            text = "amankaushik3919@gmail.com"
+            text = "amankaushiksomething@gmail.com"
         )
     }
 }
